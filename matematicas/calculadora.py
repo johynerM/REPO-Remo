a6d3e5c6b5aa939aa351121ae7c80a475c5515bd
JohynerM <johynersmc1029@gmail.com>
2026-09-21 07:30:26 -0500

    agrregue la funcionalidad de division

--- matematicas/calculadora.py
+++ matematicas/calculadora.py
@@ -6,6 +6,9 @@ def restar():
     print(num1-num2)
 def multiplicar(numero1,numero2):
     print(numero1*numero2)
+def division(numero1,numero2):
+    return(numero1/numero2)
 print(sumar(5,2))
 multiplicar(6,7)
 restar(10,6)
+division(10,1)

98000bb3ee374433f9baee79f0593df8ead92fbb
JohynerM <johynersmc1029@gmail.com>
2026-09-21 07:22:30 -0500

    Agregue la funcionalidad de restar

--- matematicas/calculadora.py
+++ matematicas/calculadora.py
@@ -1,11 +1,11 @@
 def sumar(num1,num2):
     return num1+num2
 def restar():
-    num1=int(input("ingresa el numero"))
-    num2=int(input("ingresa el siguiente numero"))
+    num1=(input("ingresa el numero"))
+    num2=(input("ingresa el siguiente numero"))
     print(num1-num2)
 def multiplicar(numero1,numero2):
     print(numero1*numero2)
 print(sumar(5,2))
 multiplicar(6,7)
-restar()
+restar(10,6)

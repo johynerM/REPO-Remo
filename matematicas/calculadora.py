2123cf88d7903e01264bd9da31303fda599bcaca
JohynerM <johynersmc1029@gmail.com>
2026-09-22 06:46:03 -0500

    editado desde main

--- matematicas/calculadora.py
+++ matematicas/calculadora.py
@@ -1,18 +1,1 @@
-def sumar(num1,num2):
-    return num1+num2
-<<<<<<< HEAD
-def restar():
-    num1=(input("ingresa el numero"))
-    num2=(input("ingresa el siguiente numero"))
-    print(num1-num2)
-def multiplicar(numero1,numero2):
-    print(numero1*numero2)
-def division(numero1,numero2):
-    return(numero1/numero2)
-print(sumar(5,2))
-multiplicar(6,7)
-restar(10,6)
-division(10,1)
-=======
-print(sumar(5,2))
->>>>>>> parent of 97b728b (Agregue la funcion de multiplica
+editando desde la rama main
